f014f640ee173fac647999dacc4df5425e729602
MathiausKalanda <mathiauskalanda1@gmail.com>
2025-03-27 18:20:20 +0000

    MK

--- src/components/ProductsHire.jsx
+++ src/components/ProductsHire.jsx
@@ -28,32 +28,33 @@ const ProductsHire = () => {
 
   return (
     <div className="">
-      <div className="bg-[#f0fff9] py-10">
+      <div className="bg-[#f0fff9] py-10 flex flex-col md:flex-row">
         {" "}
-        <div className="text-center pt-4 text-3xl font-medium capitalize">
-          Hire the best events tools from us
-        </div>
-        <div className="p-6 max-w-2xl mx-5 my-5">
-          {/* Buttons */}
-          <div className="grid grid-cols-2 md:grid-cols-4 justify-center items-center gap-4 mb-4 ">
-            {Object.keys(content).map((tab) => (
-              <div
-                key={tab}
-                onClick={() => setActiveTab(tab)}
-                className={`px-4 py-2 rounded-lg hover:bg-[#002e1d] ${
-                  activeTab === tab
-                    ? "bg-[#002e1d] text-white"
-                    : "bg-gray-300 text-black"
-                }`}
-              >
-                {tab.charAt(0).toUpperCase() + tab.slice(1)}
-              </div>
-            ))}
+        <div className="flex flex-col">
+          <div className="text-center pt-4 text-3xl font-medium capitalize">
+            Hire the best events tools from us
           </div>
-
-          {/* Content Display */}
-          <div className="p-4 rounded-lg">{content[activeTab]}</div>
+          <div className="p-6 max-w-2xl mx-5 my-5">
+            {/* Buttons */}
+            <div className="grid grid-cols-2 md:grid-cols-4 justify-center items-center gap-4 mb-4 ">
+              {Object.keys(content).map((tab) => (
+                <div
+                  key={tab}
+                  onClick={() => setActiveTab(tab)}
+                  className={`px-4 py-2 rounded-lg hover:bg-[#002e1d] ${
+                    activeTab === tab
+                      ? "bg-[#002e1d] text-white"
+                      : "bg-gray-300 text-black"
+                  }`}
+                >
+                  {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
+        {/* Content Display */}
+        <div className="md:p-4 rounded-lg md:w-1/2">{content[activeTab]}</div>
       </div>
     </div>
   );
