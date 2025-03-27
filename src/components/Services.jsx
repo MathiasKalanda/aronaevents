b9e2d86abfc5ab489298c614aba01c00ac129386
MathiausKalanda <mathiauskalanda1@gmail.com>
2025-03-27 16:57:45 +0000

    MK

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Services = () => {
   return (
-    <div className="m-4 md:m-16">
+    <div className="m-4 mx-2 md:m-16">
       <div className="">
-        <div className="m-5 mt-15">
+        <div className=" mt-15">
           <div className="text-center font-bold  my-5 text-3xl">
             Designed to Make Your Events Unforgettable, No Matter the Occasion.
             <hr className="font-bold my-1" />
@@ -19,17 +19,17 @@ const Services = () => {
             memorable experiences for you and your guests.
           </div>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-10 my-15 mx-5 max-w-2xl">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5 my-15  ">
           <div className="flex flex-col justify-center items-center bg-[#fff6ee] rounded-3xl">
             <div className="p-3 text-lg font-bold uppercase text-[#232e2a]">
               Weddings
             </div>
-            <div className="mb-4">
+            <div className="mb-4 mx-3">
               {" "}
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM9f31uwxDWjT5ym45oyCJbvEXg2cbyGker-PiaVdgbxPZ_1Q-"
                 alt=""
-                className="w-60 h-42 rounded-lg"
+                className="w-56 md:w-60 h-42 rounded-lg"
               />
             </div>
             <div className="text-center p-2 mx-3 text-md leading-5 text-[#232e2a]">
@@ -45,12 +45,12 @@ const Services = () => {
             <div className="p-3 text-lg font-bold uppercase text-[#232e2a]">
               Weddings
             </div>
-            <div className="mb-4">
+            <div className="mb-4 mx-3">
               {" "}
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM9f31uwxDWjT5ym45oyCJbvEXg2cbyGker-PiaVdgbxPZ_1Q-"
                 alt=""
-                className="w-60 h-42 rounded-lg"
+                className="w-56 md:w-60 h-42 rounded-lg"
               />
             </div>
             <div className="text-center p-2 mx-3 text-md leading-5 text-[#232e2a]">
@@ -66,12 +66,12 @@ const Services = () => {
             <div className="p-3 text-lg font-bold uppercase text-[#232e2a]">
               Weddings
             </div>
-            <div className="mb-4">
+            <div className="mb-4 mx-3">
               {" "}
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM9f31uwxDWjT5ym45oyCJbvEXg2cbyGker-PiaVdgbxPZ_1Q-"
                 alt=""
-                className="w-60 h-42 rounded-lg"
+                className="w-56 md:w-60 h-42 rounded-lg"
               />
             </div>
             <div className="text-center p-2 mx-3 text-md leading-5 text-[#232e2a]">
@@ -87,12 +87,12 @@ const Services = () => {
             <div className="p-3 text-lg font-bold uppercase text-[#232e2a]">
               Weddings
             </div>
-            <div className="mb-4">
+            <div className="mb-4 mx-3">
               {" "}
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM9f31uwxDWjT5ym45oyCJbvEXg2cbyGker-PiaVdgbxPZ_1Q-"
                 alt=""
-                className="w-60 h-42 rounded-lg"
+                className="w-56 md:w-60 h-42 rounded-lg"
               />
             </div>
             <div className="text-center p-2 mx-3 text-md leading-5 text-[#232e2a]">
@@ -104,16 +104,37 @@ const Services = () => {
               Learn More
             </button>
           </div>
-          <div className="flex flex-col justify-center items-center bg-[#fff6ee] rounded-3xl place-content-center">
+          <div className="flex flex-col justify-center items-center bg-[#fff6ee] rounded-3xl">
             <div className="p-3 text-lg font-bold uppercase text-[#232e2a]">
               Weddings
             </div>
-            <div className="mb-4">
+            <div className="mb-4 mx-3">
               {" "}
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM9f31uwxDWjT5ym45oyCJbvEXg2cbyGker-PiaVdgbxPZ_1Q-"
                 alt=""
-                className="w-60 h-42 rounded-lg"
+                className="w-56 md:w-60 h-42 rounded-lg"
+              />
+            </div>
+            <div className="text-center p-2 mx-3 text-md leading-5 text-[#232e2a]">
+              We simplify member engagement, event management, and dues
+              collection, helping associations grow and provide value to their
+              members.
+            </div>
+            <button className="bg-teal-700 py-2 px-4 mb-4 mt-7 w-3/4 mx-5 rounded-xl hover:bg-teal-900">
+              Learn More
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center bg-[#fff6ee] rounded-3xl">
+            <div className="p-3 text-lg font-bold uppercase text-[#232e2a]">
+              Weddings
+            </div>
+            <div className="mb-4 mx-3">
+              {" "}
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM9f31uwxDWjT5ym45oyCJbvEXg2cbyGker-PiaVdgbxPZ_1Q-"
+                alt=""
+                className="w-56 md:w-60 h-42 rounded-lg"
               />
             </div>
             <div className="text-center p-2 mx-3 text-md leading-5 text-[#232e2a]">
